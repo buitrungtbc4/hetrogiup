c8f23cfe55ae25be8db367e34f9db386662d05da
buitrungtbc4 <buivantrungc4@gmail.com>
2017-11-17 12:47:19 +0700

    update server.py and index.html

---
+++ server.py
@@ -0,0 +1,48 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+from  schoolsql import select_school, insert_school, update_column
+from school import School
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('index.html')
+    else:
+        return "chao anh Trung"
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['logname']
+    school = select_school(username)
+    if school is None:
+        return home()
+    if request.form['logpass'] == school.password:
+        session['logged_in'] = True
+        session['username'] = school.getUsername()
+    else:
+        flash('wrong password')
+    return home()
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    name = request.form['name']
+    username = request.form['username']
+    password = request.form['password']
+    school = School(None, name, username, password)
+    insert_school(school)
+    session['logged_in'] = True
+    session['username'] = username
+    return home()
+
+@app.route('/logout',methods=['GET'])
+def logout():
+    session['logged_in'] = False
+    session['username'] = None
+    return home()
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(debug=True)
